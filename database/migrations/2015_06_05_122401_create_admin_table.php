4ca74911996aed7c7f46f726e2f6624c7103ebfb
Bryant <bryant@weazm.com>
2015-11-17 16:09:34 +0800

    change administrator class to admin

--- database/migrations/2015_06_05_122401_create_admin_table.php
+++ database/migrations/2015_06_05_122401_create_admin_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdministratorsTable extends Migration
+class CreateAdminTable extends Migration
 {
     /**
      * Run the migrations.
