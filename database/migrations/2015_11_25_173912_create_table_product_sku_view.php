c29159fbe6cb52bc55f6294ba698174ba18e59cc
Bryant <bryant@weazm.com>
2015-11-27 11:49:03 +0800

    add product_sku_view

---
+++ database/migrations/2015_11_25_173912_create_table_product_sku_view.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProductSkuView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("CREATE VIEW product_sku_view as SELECT
+
+            product_sku.id AS id,
+            product_sku.sku_no AS sku_no,
+            product_sku.stock AS stock,
+            product_sku.sales AS sales,
+            product_sku.price AS price,
+            products.merchant_id AS merchant_id,
+            products.title AS title,
+            products.member_discount AS member_discount,
+            products.category_id AS category_id,
+            products.cover_image AS cover_image,
+            group_concat(distinct attributes.id, ':', attributes.name, '=>', attribute_values.id, ':', attribute_values.value separator ',') AS attributes
+            FROM product_sku
+                LEFT JOIN products ON product_sku.product_id = products.id
+                LEFT JOIN sku_attribute_value ON product_sku.id = sku_attribute_value.product_sku_id
+                LEFT JOIN attribute_values ON sku_attribute_value.attribute_value_id = attribute_values.id
+                LEFT JOIN attributes ON attribute_values.attribute_id = attributes.id
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP VIEW product_sku_view');
+    }
+}
