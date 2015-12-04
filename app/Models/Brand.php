1127655cd9eeca76dfd051ee172d7b0228fea1fa
Bryant <bryant@weazm.com>
2015-12-04 17:41:15 +0800

    finish brands

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Brand extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'cover_image'];
+
+    public function products()
+    {
+        return $this->hasMany('App\Models\Product');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Models\Category', 'category_brand');
+    }
 }
