a1c08f67f9a80805c0d222d6e0c59ce491a415b4
Bryant <bryant@weazm.com>
2015-12-04 18:17:19 +0800

    update comments

--- app/Services/Product/Comment/CommentRepository.php
+++ app/Services/Product/Comment/CommentRepository.php
@@ -10,36 +10,82 @@ namespace App\Services\Product\Comment;
 
 
 use App\Models\Comment;
+use Pheanstalk\Exception;
 
+/**
+ * Class CommentsRepository
+ * @package App\Services\Product\Comment
+ */
 class CommentsRepository
 {
     /**
      * create a comment
      * @param $data
+     *  - (string) content : 评论内容
+     *  - (integer) user_id
+     *  - (integer) product_id
+     *  - (integer) order_id
+     *  - (bool) status : 评论状态, 1 为 正常, 0 为不显示
+     *  - (array) image_ids: 图片id
+     * @return static
      */
     public static function create($data)
     {
         try {
             DB::beginTransaction();
 
-            $comment = new Comment;
-
-            $comment->content = $data['content'];
-            $comment->user_id = $data['user_id'];
-            $comment->product_id = $data['product_id'];
-            $comment->order_id = $data['order_id'];
-            $comment->save();
-
-            if (count($data['image_ids']) > 0) {
-                $comment->images()->attach($data['image_ids']);
+            if (isset($data['image_ids'])) {
+                $image_ids = $data['image_ids'];
             }
+
+            $data = array_only($data, ['content', 'user_id', 'product_id', 'order_id']);
+
+            $record = Comment::where('user_id', $data['user_id'])->where('order_id', $data['order_id'])->count();
+
+            if ($record > 0) {
+                throw new Exception('user already commented for the order');
+            }
+
+            $comment = Comment::create($data);
+
+            if (count($image_ids) > 0) {
+                $comment->images()->attach($image_ids);
+            }
+
+            return $comment;
 
             DB::commit();
         } catch (Exception $e) {
-            DB::rollBack();
+            return $e->getMessage();
         }
     }
 
+    /**
+     * @param $id
+     * @param $data
+     * @return int|string
+     */
+    public static function update($id, $data)
+    {
+        try {
+            $comment = Comment::find($id);
+            if (!$comment) {
+                throw new Exception("COMMENT NOT FOUND");
+            }
+            $data = array_only($data, ['status', 'content']);
+            $comment->update($data);
+
+            return 1;
+        } catch (Exception $e) {
+
+            return $e->getMessage();
+        }
+    }
+
+    /**
+     * @param $id
+     * @return bool
+     */
     public static function delete($id)
     {
         try {
