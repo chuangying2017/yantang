4eea736a9c42dc6d6c8159dff7c4c7c5cefa19cb
Bryant <bryant@weazm.com>
2015-12-08 11:44:33 +0800

    add merchant table

---
+++ database/migrations/2015_12_08_113028_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('mobile');
+            $table->timestamp('birthday');
+            $table->string('avatar');
+            $table->string('sex');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clients');
+
+    }
+
+}
