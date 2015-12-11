c6a4ea3ab97e2b94b55cc78dc5fa2693ce9b4503
troyz <troy@weazm.com>
2015-12-12 04:49:04 +0800

    add seeder

---
+++ database/seeds/BrandSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Brand;
+use Illuminate\Database\Seeder;
+
+class BrandSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Brand::truncate();
+        $data = [
+            ['name' => '芭妮兰'],
+            ['name' => 'benefit'],
+            ['name' => 'maskhouse'],
+            ['name' => '兰蔻'],
+            ['name' => 'SNP'],
+            ['name' => '兰芝'],
+            ['name' => 'SK-II'],
+            ['name' => '拱辰享'],
+            ['name' => '纪梵希'],
+            ['name' => '贝德玛'],
+            ['name' => '赫拉'],
+            ['name' => '阿玛尼'],
+            ['name' => '香奈儿'],
+        ];
+
+        Brand::insert($data);
+    }
+}
