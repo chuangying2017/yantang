79b1830b2bff943acd49e3c5949caa4f70b69975
troyz <troy@weazm.com>
2015-12-12 06:24:59 +0800

    add cors

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         Dingo\Api\Provider\LaravelServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
+
     ],
 
     /*
