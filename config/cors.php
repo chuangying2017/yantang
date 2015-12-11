d4db01d67f6c99933b76af00ec11db28e1c7684d
troyz <troy@weazm.com>
2015-12-12 06:24:59 +0800

    add cors

--- config/cors.php
+++ config/cors.php
@@ -7,16 +7,16 @@ return [
      |--------------------------------------------------------------------------
      |
 
-     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*') 
+     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
      | to accept any value, the allowed methods however have to be explicitly listed.
      |
      */
-    'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
-    'allowedHeaders' => ['*'],
-    'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE'],
-    'exposedHeaders' => [],
-    'maxAge' => 0,
-    'hosts' => [],
+    'supportsCredentials' => true,
+    'allowedOrigins'      => ['*'],
+    'allowedHeaders'      => ['Content-Type', 'Accept', 'Authorization', 'X-Requested-With'],
+    'allowedMethods'      => ['GET', 'POST', 'PUT', 'DELETE'],
+    'exposedHeaders'      => ['Authorization'],
+    'maxAge'              => 0,
+    'hosts'               => [],
 ];
 
