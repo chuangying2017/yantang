d60be554111ae3961d9d7071dbe7a64d6ce65683
troyz <troy@weazm.com>
2015-12-12 07:41:52 +0800

    修改路由

---
+++ app/Services/Home/NavService.php
@@ -0,0 +1,17 @@
+<?php namespace App\Services\Home;
+
+use App\Models\Nav;
+
+class NavService {
+
+    public static function nav()
+    {
+        return Nav::get(['id', 'name', 'type', 'url']);
+    }
+
+    public static function sections()
+    {
+
+    }
+
+}
