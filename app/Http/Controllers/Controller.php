ecdcf09d28c0e7592ffd8f8a967e53dd945bb846
troyz <troy@weazm.com>
2015-12-12 01:54:42 +0800

    add jwt un test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,16 +9,25 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
-abstract class Controller extends BaseController
-{
+abstract class Controller extends BaseController {
+
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiHelpers, ApiFormatHelpers, Helpers;
+
 
     public function getCurrentAuthUserId()
     {
-        #todo @troy get user from jwt
-        $user_id = 1;
-        return $user_id;
+        $user = $this->getCurrentAuthUser();
+
+        return $user['id'];
+    }
+
+    public function getCurrentAuthUser()
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+
+        return $user;
     }
 
 }
--- app/Http/Routes/Frontend/Api.php
+++ app/Http/Routes/Frontend/Api.php
@@ -1,30 +1,59 @@
 <?php
 
 
-$api->resource('categories', 'CategoryController', ['only' => ['index', 'show']]);
+/**
+ * Frontend Access Controllers
+ */
+$api->group(['namespace' => 'Auth'], function () use ($api) {
+    /**
+     * These routes require the user to be logged in
+     */
+    $api->group(['middleware' => 'jwt.refresh'], function () {
+        get('auth/logout', 'AuthController@getLogout');
+        get('auth/password/change', 'PasswordController@getChangePassword');
+        post('auth/password/change', 'PasswordController@postChangePassword')->name('password.change');
+    });
 
-$api->resource('products', 'ProductController', ['only' => 'index', 'show']);
+    /**
+     * These reoutes require the user NOT be logged in
+     */
+    $api->group(['middleware' => 'guest'], function () use ($api) {
+        get('auth/login/{provider}', 'AuthController@loginThirdParty')->name('auth.provider');
+        get('account/confirm/{token}', 'AuthController@confirmAccount')->name('account.confirm');
+        get('account/confirm/resend/{user_id}', 'AuthController@resendConfirmationEmail')->name('account.confirm.resend');
 
-$api->resource('fav', 'FavController', ['only' => ['index', 'store', 'destroy']]);
-
-
-$api->resource('carts', 'CartController', ['only' => ['index', 'store', 'update', 'destroy']]);
-
-
-$api->group(['namespace' => 'Marketing', 'prefix' => 'marketing'], function ($api) {
-    $api->resource('coupons', 'CouponController');
-    $api->post('coupons/exchange', [
-        'as'   => 'api.marketing.coupons.exchange',
-        'uses' => 'CouponController@exchange'
-    ]);
+        $api->controller('auth', 'AuthController');
+        $api->controller('password', 'PasswordController');
+    });
 });
 
-$api->post('orders/confirm', [
-    'as'   => 'order.preConfirm',
-    'uses' => 'OrderController@preConfirm'
-]);
 
-$api->resource('orders', 'OrderController');
-$api->resource('address', 'AddressController');
+$api->resource('categories', 'CategoryController', ['only' => ['index', 'show']]);
+$api->resource('products', 'ProductController', ['only' => 'index', 'show']);
+
+
+/**
+ * 需要登录才能查看
+ */
+$api->group(['middleware' => 'jwt.refresh'], function ($api) {
+    $api->group(['namespace' => 'Marketing', 'prefix' => 'marketing'], function ($api) {
+        $api->resource('coupons', 'CouponController');
+        $api->post('coupons/exchange', [
+            'as'   => 'api.marketing.coupons.exchange',
+            'uses' => 'CouponController@exchange'
+        ]);
+    });
+
+    $api->resource('fav', 'FavController', ['only' => ['index', 'store', 'destroy']]);
+    $api->resource('carts', 'CartController', ['only' => ['index', 'store', 'update', 'destroy']]);
+    $api->post('orders/confirm', [
+        'as'   => 'order.preConfirm',
+        'uses' => 'OrderController@preConfirm'
+    ]);
+    $api->resource('orders', 'OrderController');
+    $api->resource('address', 'AddressController');
+});
+
+
 
 
