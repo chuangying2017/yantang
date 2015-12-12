5babafcced3ad967b1ee59d900cf9c9082ad1afb
troyz <troy@weazm.com>
2015-12-12 11:56:27 +0800

    fix

--- app/Http/Controllers/Frontend/Api/OrderController.php
+++ app/Http/Controllers/Frontend/Api/OrderController.php
@@ -99,7 +99,6 @@ class OrderController extends Controller {
 
             $order = $this->orderGenerator->confirm($uuid, $address_id);
 
-            return $order;
             return $this->response->created(route('api.orders.show', $order['order_no']));
         } catch (\Exception $e) {
             $this->response->errorInternal($e->getMessage());
