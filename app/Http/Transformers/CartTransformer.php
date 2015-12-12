6de8e2cbd79b743d71d150a28484dcaafd091155
troyz <troy@weazm.com>
2015-12-12 10:26:14 +0800

    修改购物车

--- app/Http/Transformers/CartTransformer.php
+++ app/Http/Transformers/CartTransformer.php
@@ -8,22 +8,30 @@ class CartTransformer extends TransformerAbstract {
 
     public function transform(Cart $cart)
     {
+        $detail = [];
         if (isset($cart->product_sku)) {
-            $this->setDefaultIncludes(['sku']);
+            $productSku = $cart->product_sku;
+            $detail = [
+                'product_id'  => (int)$productSku->product_id,
+                'sku_no'      => $productSku->sku_no,
+                'stocks'      => (int)$productSku->stock,
+                'sales'       => (int)$productSku->sales,
+                'price'       => (int)$productSku->price,
+                'merchant_id' => (int)$productSku->merchant_id,
+                'title'       => $productSku->title,
+                'category_id' => (int)$productSku->category_id,
+                'cover_image' => $productSku->cover_image,
+                'attributes'  => $productSku->attributes ? json_decode('[' . $productSku->attributes . ']', true) : null
+            ];
         }
 
-        return [
+        return array_merge([
             'id'             => (int)$cart->id,
             'product_sku_id' => (int)$cart->product_sku_id,
             'quantity'       => (int)$cart->quantity,
-        ];
+            'status'         => (int)$cart->status,
+        ], $detail);
     }
 
-    public function includeSku(Cart $cart)
-    {
-        $sku = $cart->product_sku;
-
-        return $this->item($sku, new ProductSkuTransformer());
-    }
 
 }
