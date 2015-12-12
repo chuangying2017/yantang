a9cdd5ed14e630e7a8a446e6c8b23fdfe3a58bd1
troyz <troy@weazm.com>
2015-12-12 11:56:27 +0800

    fix

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,11 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cart extends Model {
 
-    use SoftDeletes;
+
     protected $table = 'cart';
 
     protected $guarded = ['id'];
