3072d3b4ad5449d934c24dfec3e00194d89b579e
Bryant <bryant@weazm.com>
2015-12-15 10:56:29 +0800

    temp save

--- app/Http/Controllers/Frontend/Api/IndexController.php
+++ app/Http/Controllers/Frontend/Api/IndexController.php
@@ -11,7 +11,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class IndexController extends Controller {
+class IndexController extends Controller
+{
 
     /**
      * Display a listing of the resource.
--- app/Http/Routes/Frontend/Api.php
+++ app/Http/Routes/Frontend/Api.php
@@ -28,22 +28,22 @@ $api->group(['namespace' => 'Auth'], function () use ($api) {
 
 
 $api->get('nav', [
-    'as'   => 'home.nav',
+    'as' => 'home.nav',
     'uses' => 'IndexController@getNav'
 ]);
 
 $api->get('banners', [
-    'as'   => 'home.banners',
+    'as' => 'home.banners',
     'uses' => 'IndexController@getBanners'
 ]);
 
 $api->get('sections', [
-    'as'   => 'home.sections',
+    'as' => 'home.sections',
     'uses' => 'IndexController@getSections'
 ]);
 
 $api->get('user/info', [
-    'as'   => 'client.user.info',
+    'as' => 'client.user.info',
     'uses' => 'IndexController@getUserInfo'
 ]);
 
@@ -56,33 +56,33 @@ $api->resource('products', 'ProductController', ['only' => ['index', 'show']]);
 /**
  * 需要登录才能查看
  */
-//$api->group(['middleware' => 'jwt.refresh'], function ($api) {
+$api->group(['middleware' => 'jwt.refresh'], function ($api) {
 
-$api->group(['namespace' => 'Marketing', 'prefix' => 'marketing'], function ($api) {
-    $api->resource('coupons', 'CouponController');
-    $api->post('coupons/exchange', [
-        'as'   => 'api.marketing.coupons.exchange',
-        'uses' => 'CouponController@exchange'
+    $api->group(['namespace' => 'Marketing', 'prefix' => 'marketing'], function ($api) {
+        $api->resource('coupons', 'CouponController');
+        $api->post('coupons/exchange', [
+            'as' => 'api.marketing.coupons.exchange',
+            'uses' => 'CouponController@exchange'
+        ]);
+    });
+
+    $api->resource('user/favs', 'FavController', ['only' => ['index', 'store', 'destroy']]);
+    $api->resource('cart', 'CartController', ['only' => ['index', 'store', 'update', 'destroy']]);
+
+    $api->post('orders/confirm', [
+        'as' => 'order.preConfirm',
+        'uses' => 'OrderController@preConfirm'
     ]);
+
+    $api->post('orders/request', [
+        'as' => 'order.fetchConfirm',
+        'uses' => 'OrderController@fetchConfirm'
+    ]);
+
+
+    $api->resource('orders', 'OrderController');
+    $api->resource('address', 'AddressController');
 });
-
-$api->resource('user/favs', 'FavController', ['only' => ['index', 'store', 'destroy']]);
-$api->resource('cart', 'CartController', ['only' => ['index', 'store', 'update', 'destroy']]);
-
-$api->post('orders/confirm', [
-    'as'   => 'order.preConfirm',
-    'uses' => 'OrderController@preConfirm'
-]);
-
-$api->post('orders/request', [
-    'as'   => 'order.fetchConfirm',
-    'uses' => 'OrderController@fetchConfirm'
-]);
-
-
-$api->resource('orders', 'OrderController');
-$api->resource('address', 'AddressController');
-//});
 
 
 
