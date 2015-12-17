d46397dc7120ec10a2360f0a2e893ffc83c89f8e
will <iamhandian@gmail.com>
2015-12-17 16:34:40 +0800

    添加订单详情页面

--- app/Http/Controllers/Backend/Product/GroupController.php
+++ app/Http/Controllers/Backend/Product/GroupController.php
@@ -19,7 +19,7 @@ class GroupController extends Controller {
 
     public function create()
     {
-    	return view('backend.merchant.create');
+    	return view('backend.orders.detail');
         // return view('backend.groups.create');
     }
 }
