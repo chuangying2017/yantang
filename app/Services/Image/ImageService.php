ea1795c113d413d1e674f7f63c09047d755a43d9
troyz <troy@weazm.com>
2015-12-18 14:02:25 +0800

    添加图片管理api

--- app/Services/Image/ImageService.php
+++ app/Services/Image/ImageService.php
@@ -10,37 +10,35 @@ namespace App\Services\Image;
 
 
 use App\Models\Merchant;
+use App\Services\ApiConst;
 
-class ImageService
-{
+class ImageService {
 
-    public static function getToken()
+    public static function getToken($merchant_id)
     {
-        return ImageRepository::getToken($callback_url, $merchat_id);
+        $callback_url = api_route('qiniu.callback');
+
+        return ImageRepository::getToken($callback_url, $merchant_id);
     }
 
-    /**
-     *
-     * @param $data
-     */
-    public static function callback($data)
+    public static function create($media_id, $merchant_id)
     {
-        //todo@bryant handle callback
+        return ImageRepository::create($merchant_id, $media_id);
     }
 
     /**
      * @param $ids array|integer
      */
-    public static function delete($ids)
+    public static function delete($ids, $merchant_id)
     {
-        return ImageRepository::delete($ids);
+        ImageRepository::delete($ids, $merchant_id);
     }
 
     public static function getByMerchant($merchant_id)
     {
         $merchant = Merchant::findOrFail($merchant_id);
 
-        return $merchant->images()->get();
+        return $merchant->images()->paginate(ApiConst::IMAGE_PER_PAGE);
     }
 
 
