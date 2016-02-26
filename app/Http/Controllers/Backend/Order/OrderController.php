6ebe648b3984e4bbd0af6eca5afc5ca81275cc3a
Bryant <bryant@weazm.com>
2016-02-26 19:59:44 +0800

    temp save

--- app/Http/Controllers/Backend/Order/OrderController.php
+++ app/Http/Controllers/Backend/Order/OrderController.php
@@ -22,8 +22,10 @@ class OrderController extends Controller
         //todo@bryant: wait for api
         $page = $request->get('page') ?: '';
         $status = $request->get('status') ?: '';
+        $keyword = $request->get('keyword') ?: '';
         try {
-            $records = $this->api->get('api/admin/orders?page=' . $page . '&status=' . $status);
+            $records = $this->api->get('api/admin/orders?page=' . $page . '&status=' . $status . '&keyword=' . $keyword);
+            dd($records);
             javascript()->put([
                 'config' => [
                     'api_url' => url('api/'),
--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -36,7 +36,7 @@
             <li class="{{ Active::pattern('admin/dashboard') }}"><a
                     href="{!!route('backend.dashboard')!!}"><span>{{ trans('menus.dashboard') }}</span></a></li>
 
-            <li class="{{ Active::pattern('admin/dashboard') }}">
+            <li class="active">
                 <a href=""><span>{{ trans('menus.product.management') }}</span><i
                         class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
@@ -44,7 +44,7 @@
                                 class="fa fa-circle-o"></i>{{ trans('menus.product.all') }}</a></li>
                     <li><a href="{!!url('admin/groups')!!}"><i
                                 class="fa fa-circle-o"></i>{{ trans('menus.product.groups') }}</a></li>
-                    <li>
+                    <li class="active">
                         <a href="{!!url('admin/brands')!!}"><i
                                 class="fa fa-circle-o"></i>品牌管理<i
                                 class="fa fa-angle-left pull-right"></i></a>
