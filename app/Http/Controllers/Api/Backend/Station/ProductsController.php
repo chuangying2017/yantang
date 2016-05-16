3a9e5ef850930a6fe0a5579767e5b3e12028fb7b
Ares <thr@weazm.com>
2016-05-16 15:57:08 +0800

    update

---
+++ app/Http/Controllers/Api/Backend/Station/ProductsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\Backend\Station;
+
+
+use App\Http\Controllers\Controller;
+use App\Http\Transformers\BackendStationProductTransformer;
+
+class ProductController extends Controller
+{
+
+    /**
+     * 查看可定购商品
+     */
+    public function index()
+    {
+
+    }
+}
--- app/Http/api_routes.php
+++ app/Http/api_routes.php
@@ -4,9 +4,19 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
 
-    $api->group(['namespace' => 'App\V1\Controllers', 'middleware' => 'cors'], function ($api) {
+    $api->group(['namespace' => 'App\Http\Controllers\Api', 'middleware' => 'cors'], function ($api) {
+        /**
+         * 后台
+         */
+        $api->group(['namespace' => 'Backend'], function ($api) {
+            /**
+             * 服务部
+             */
+            $api->group(['namespace' => 'Station', 'prefix' => 'stations'], function ($api) {
 
-
+                $api->resource('products', 'ProductsController', ['only' => ['index']]); //查看可定购商品
+            });
+        });
     });
 
 });
