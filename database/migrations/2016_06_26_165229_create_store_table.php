5ebd2fa156f2f1153f5c8fa51bc48f190b49c3a9
troyz <troy@weazm.com>
2016-06-26 16:53:51 +0800

    测试order tickets

--- database/migrations/2016_06_26_165229_create_store_table.php
+++ database/migrations/2016_06_26_165229_create_store_table.php
@@ -14,6 +14,17 @@ class CreateStoreTable extends Migration
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->string('store_no')->nullable();
+            $table->string('name');
+            $table->string('address');
+            $table->string('cover_image');
+            $table->string('director', 45);
+            $table->string('phone', 15);
+            $table->string('tel', 15);
+            $table->string('longitude', 30);
+            $table->string('latitude', 30);
+            $table->tinyInteger('active')->default(1);
             $table->timestamps();
         });
     }
