5310c59b96f56bfea4b2be0e50e0ec880c782356
troyz <troy@weazm.com>
2016-07-05 18:16:45 +0800

    add store soft delete

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use App\Models\Access\User\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Store extends Model {
+
+    use SoftDeletes;
 
     protected $table = 'stores';
 
