ed4cd34d6b146ad86c80391ec8c25ac94486b5f9
troyz <troy@weazm.com>
2016-07-19 17:54:16 +0800

    fix table

--- database/migrations/2016_07_19_170837_add_street_to_preorders_table.php
+++ database/migrations/2016_07_19_170837_add_street_to_preorders_table.php
@@ -13,7 +13,7 @@ class AddStreetToPreordersTable extends Migration
     public function up()
     {
         Schema::table('preorders', function (Blueprint $table) {
-            //
+            $table->string('street');
         });
     }
 
@@ -25,7 +25,7 @@ class AddStreetToPreordersTable extends Migration
     public function down()
     {
         Schema::table('preorders', function (Blueprint $table) {
-            //
+            $table->dropColumn('street');
         });
     }
 }
