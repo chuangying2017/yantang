bd3eb601e326ef0e28794c35d27fbe0c5ebc83a1
troyz <troy@weazm.com>
2016-07-21 21:00:55 +0800

    修改客户端回调地址

--- config/services.php
+++ config/services.php
@@ -73,7 +73,7 @@ return [
         'redirect' => env('WECHAT_REDIRECT_URL'),
         'auth_base_uri' => 'https://open.weixin.qq.com/connect/oauth2/authorize',
         'redirect_urls' => [
-            'client' => 'http://client.yt.weazm.com/auth/wechatAuth',
+            'client' => 'http://client.yt.weazm.com/?#!/auth/wechatAuth',
             'station' => 'http://station.yt.weazm.com/auth/wechatAuth',
             'store' => 'http://store.yt.weazm.com/auth/wechatAuth',
             'staff' => 'http://staff.yt.weazm.com/auth/wechatAuth',
