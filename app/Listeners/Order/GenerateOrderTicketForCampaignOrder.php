1e1d7e61c59e9be796ea96ef6c690322239e6e71
skys215 <skys215@gmail.com>
2017-04-19 18:07:31 +0800

    Dispatch collect order ticket

--- app/Listeners/Order/GenerateOrderTicketForCampaignOrder.php
+++ app/Listeners/Order/GenerateOrderTicketForCampaignOrder.php
@@ -6,6 +6,7 @@ use App\Events\Order\OrderIsPaid;
 use App\Services\Order\OrderProtocol;
 use App\Services\OrderTicket\OrderTicketManageContract;
 use App\Services\Order\OrderManageContract;
+use App\Services\Promotion\PromotionProtocol;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -22,15 +23,27 @@ class GenerateOrderTicketForCampaignOrder {
     private $orderManage;
 
     /**
+     * @var CouponService
+     */
+    private $couponService;
+
+    /**
+     * @var CouponRepositoryContract
+     */
+    private $couponRepo;
+
+    /**
      * Create the event listener.
      *
      * @return void
      */
     public function __construct(OrderTicketManageContract $orderTicketManage,
-       OrderManageContract $orderManage)
+       OrderManageContract $orderManage, CouponService $couponService, CouponRepositoryContract $couponRepo)
     {
         $this->orderTicketManage = $orderTicketManage;
         $this->orderManage = $orderManage;
+        $this->couponService = $couponService;
+        $this->couponRepo = $couponRepo;
     }
 
     /**
@@ -47,6 +60,19 @@ class GenerateOrderTicketForCampaignOrder {
         }
         else if( $order['order_type'] == OrderProtocol::ORDER_TYPE_OF_COLLECT) {
             $this->orderManage->orderDone($order['id']);
+
+            try{
+                //送券
+                $coupons = $this->couponRepo->getAllByQualifyTye(PromotionProtocol::QUALI_TYPE_OF_COLLECT_ORDER);
+                if (count($coupons)) {
+                    foreach ($coupons as $coupon) {
+                        $result = $this->couponService->dispatchWithoutCheck($user['id'], $coupon['id']);
+                    }
+                }
+            }
+            catch( \Exception $e ){
+                \Log::error( 'Failed to dispatch ticket when collect order has finished. '.$e );
+            }
         }
     }
 }
