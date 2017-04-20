e1421972e50b0d9c0280745c54e68b38e4de9a6d
skys215 <skys215@gmail.com>
2017-04-20 15:37:54 +0800

    Return order for collect_order

--- app/Api/V1/Transformers/CollectOrderTransformer.php
+++ app/Api/V1/Transformers/CollectOrderTransformer.php
@@ -7,6 +7,7 @@ class CollectOrderTransformer extends TransformerAbstract {
     public function transform(CollectOrder $collect_order)
     {
         $data = $collect_order->toArray();
+        $data['order'] = $collect_order->order;
         $data['address'] = $collect_order->address;
         $data['sku'] = $collect_order->sku;
         return $data;
