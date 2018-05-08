91e9a4c37cef3300b3f9829a339dbb89a9128d03
chuangying2017 <631711487@qq.com>
2018-05-08 12:01:07 +0800

    111111

---
+++ app/Api/V1/Controllers/Admin/Others/Protocols.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Api\V1\Controllers\Admin\Others;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class Protocols extends Controller
+{
+    //展示数据
+    public function index(){
+
+    }
+}
