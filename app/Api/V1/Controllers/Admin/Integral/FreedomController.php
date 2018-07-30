ec4ef8b5d8cc3f392f9f3b1360956546ad6a4ed2
chuangying2017 <631711487@qq.com>
2018-07-30 19:40:16 +0800

    发货管理 展示

--- app/Api/V1/Controllers/Admin/Integral/FreedomController.php
+++ app/Api/V1/Controllers/Admin/Integral/FreedomController.php
@@ -18,6 +18,17 @@ class FreedomController extends Controller
         $this->shipping = $orderIntegral;
     }
 
+    /**
+     * @api {get} /admin/integral/freedomThe/shippingManagement Request shipping management information
+     * @apiName GetUser
+     * @apiGroup User
+     *
+     * @apiParam {Number} keywords 关键字.
+     * @apiParam {date} start_time  开始时间
+     * @apiParam {date} end_time    结束时间
+     *
+     * @apiSuccess {array} arrayName 请求成功就是一个数据集数组或者空的数组.
+     */
     public function Shipping_management(Request $request) //发货 管理
     {
 
@@ -26,6 +37,14 @@ class FreedomController extends Controller
         return $this->response->paginator($gain_data, new AdminShipmentsTransformer())->setStatusCode(201);
     }
 
+    /**
+     * @api {get} /admin/integral/freedomThe/{id}/shippingOrderDetail 发货详情
+     * @apiName GetIntegral
+     * @apiGroup Integral
+     *
+     *
+     * @apiSuccess {array} arrayName 请求成功就是一个数据集数组或者空的数组.
+     */
     public function Shipping_order_detail($id)
     {
         $order_detail = $this->shipping->first($id);
---
+++ dome/src/api.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @api {get} /admin/integral/freedomThe/shippingManagement 发货管理
+ * @apiName GetManager
+ * @apiGroup Integral
+ *
+ * @apiParam {Number} keywords  关键字
+ * @apiParam {date} start_time  开始时间
+ * @apiParam {date} end_time    结束时间
+ *
+ * @apiSuccess {array} arrayName 请求成功就是一个数据集数组或者空的数组.
+ */
+
+/**
+ * @api {get} /admin/integral/freedomThe/{id}/shippingOrderDetail 发货详情
+ * @apiName GetIntegral
+ * @apiGroup Integral
+ *
+ *
+ * @apiSuccess {array} arrayName 请求成功就是一个数据集数组或者空的数组.
+ */
