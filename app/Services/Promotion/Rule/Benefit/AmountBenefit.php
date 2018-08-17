16f1d7b8adb27be522907ffc44b9bdf3ed1da980
chuangying2017 <631711487@qq.com>
2018-08-17 10:00:42 +0800

    修复使用优惠券错误

--- app/Services/Promotion/Rule/Benefit/AmountBenefit.php
+++ app/Services/Promotion/Rule/Benefit/AmountBenefit.php
@@ -6,6 +6,10 @@ class AmountBenefit extends Benefit {
 
     public function cal($mode, $value, PromotionAbleItemContract $items, $item_keys = null)
     {
+        if (is_object($item_keys))
+        {
+            $item_keys = $item_keys->toArray();
+        }
         \Cache::put('item_keys',$item_keys, 200);
         $benefit_value = self::calModeValue($mode, $items->getAmount(array_keys($item_keys)), $value);
 
