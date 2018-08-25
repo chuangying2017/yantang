1f20863f8025fa1e0c34f61263552e9b7650a205
chuangying2017 <631711487@qq.com>
2018-08-25 14:52:03 +0800

    更新浏览量

--- app/Api/V1/Transformers/Integral/Admin/IntegralProductSkuTransformer.php
+++ app/Api/V1/Transformers/Integral/Admin/IntegralProductSkuTransformer.php
@@ -22,6 +22,7 @@ class IntegralProductSkuTransformer extends TransformerAbstract {
                 'convert_unit'      =>$productSku['convert_unit'],//兑换单位
                 'convert_day'       =>$productSku['convert_day'],//兑换天数
                 'remainder'         =>$productSku['remainder'],
+                'browse_num'        =>$productSku['browse_num']
             ];
     }
 }
