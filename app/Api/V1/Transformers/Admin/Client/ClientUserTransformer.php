abf10f184dd12556937fc6c2c61bdc210a43d400
chuangying2017 <631711487@qq.com>
2018-09-25 10:42:38 +0800

    后天展示新会员

--- app/Api/V1/Transformers/Admin/Client/ClientUserTransformer.php
+++ app/Api/V1/Transformers/Admin/Client/ClientUserTransformer.php
@@ -1,12 +1,24 @@
 <?php namespace App\Api\V1\Transformers\Admin\Client;
 use App\Models\Client\Client;
+use App\Repositories\Member\ReposFile\MemberLimitRepository;
 use League\Fractal\TransformerAbstract;
 
 class ClientUserTransformer extends TransformerAbstract{
 
+
     public function transform(Client $client)
     {
-        return $client->toArray();
+        $data = $client->toArray();
+
+        $data['new_member'] = $this->new_member($data['user_id']);
+
+        return $data;
     }
 
+    public function new_member($userId)
+    {
+        $member = new MemberLimitRepository();
+
+        return $member->new_member($userId);
+    }
 }
